e23417c354c379b743781913d2d32823571c27a9
Daniel Camargo <97453311+DanielCamargo1@users.noreply.github.com>
2022-08-18 09:32:26 -0300

    commit int
    O primeiro commit do site

---
+++ habilidades.js
@@ -0,0 +1,62 @@
+function entrarp(){
+    var tecnologia = document.getElementById('py')
+    var res = document.getElementById('res')
+    res.innerHTML = 'Um ano de contato com a tecnologia'
+}
+
+function sairp(){
+    var tecnologia = document.getElementById('py')
+    var res = document.getElementById('res')
+    res.innerHTML = ''
+}
+
+function entrarh(){
+    var tecnologia = document.getElementById('h')
+    var res = document.getElementById('resh')
+    res.innerHTML = 'Cinco meses de contato com a técnologia'
+}
+
+function sairh(){
+    var tecnologia = document.getElementById('h')
+    var res = document.getElementById('resh')
+    res.innerHTML = ''
+}
+
+function entrarg(){
+    var tecnologia = document.getElementById('g')
+    var res = document.getElementById('resg')
+    res.innerHTML ='Um ano de contato com a técnologia'
+
+}
+
+function sairg(){
+    var tecnologia = document.getElementById('g')
+    var res = document.getElementById('resg')
+    res.innerHTML=''
+}
+
+function entrarc(){
+    var tecnologia = document.getElementById('g')
+    var res = document.getElementById('resc')
+    res.innerHTML ='Cinco meses de contato com a técnologia'
+
+}
+
+function sairc(){
+    var tecnologia = document.getElementById('g')
+    var res = document.getElementById('resc')
+    res.innerHTML=''
+}
+
+function entrarj(){
+    var tecnologia = document.getElementById('j')
+    var res = document.getElementById('resj')
+    res.innerHTML ='Cico meses de contato com a técnologia'
+
+}
+
+function sairj(){
+    var tecnologia = document.getElementById('j')
+    var res = document.getElementById('resj')
+    res.innerHTML=''
+}
---
+++ index.js
@@ -0,0 +1,9 @@
+function Entrar(){
+    var botton = document.getElementById('button')    
+    botton.style.background = '#13293d'
+}
+
+function sair(){
+    var botton = document.getElementById('button')
+    botton.style.background = "#006494"
+}
